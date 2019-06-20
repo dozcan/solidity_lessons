ccd6e95ea15ce2d1a84b7bf018419ae381612324
doga ozcan <dogasava@gmail.com>
2019-06-20 14:13:20 +0300

    Update storage.sol

--- storage.sol
+++ storage.sol
@@ -1,3 +1,20 @@
+REFERANS VE VALUE TYPE ATAMALARI
+
+1) value degerler ister, storage ister memory olsun birbirine atanırken yeni bir copy değişken oluşturulduğu için birine yapılan
+değişiklik diğerini etkilemez
+
+2) iki storage referans değeri arasındaki atamalarda yeni bir copy referans değer oluşturulduğu için birine yapılan değişiklik diğerini
+etkilemez
+
+3) bir storage referans ile bir memry referans değeri arasındaki atamalarda yeni bir copy referans değer oluşturulduğu için birine yapılan değişiklik diğerini
+etkilemez
+
+4) iki memory referans değerleri arasındaki atamalarda yeni bir copy işlemi olmadığı için birine yapılan değişiklik diğerine yansır
+
+
+***** sonuç olarak iki memory referans arasında yeni bir copy değişken oluşturulmaz
+
+
 pragma solidity ^0.4.0;
 
 contract C {
@@ -10,9 +27,6 @@ contract C {
         data = x;
     }
 }
-
-
-
 
 
 pragma solidity ^0.4.0;
