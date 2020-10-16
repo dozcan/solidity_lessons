3d4f65b375135725f09f92ed60322b4e766d0d5c
doga ozcan <dogasava@gmail.com>
2020-10-16 17:38:04 +0300

    Update array.sol

--- array.sol
+++ array.sol
@@ -9,3 +9,37 @@ bunun yerine tek tek atama yapabiliriz
         x[0] = 1;
         x[1] = 3;
         x[2] = 4;
+        
+//////////////////////////////
+        
+pragma solidity >=0.2.0 <0.8.0;
+
+contract compare {
+    
+    uint[2**20] m_aLotOfIntegers;
+    uint[2][2] m_pairsOfFlags;
+    uint[] ss;
+
+ 
+    function set(uint value) public returns (uint[2][2] memory){
+        m_pairsOfFlags[0][0]= value;
+        m_pairsOfFlags[0][1]= value;
+        m_pairsOfFlags[1][0]= value+1;
+        m_pairsOfFlags[1][1]= value+1;
+        return m_pairsOfFlags;
+    }
+  
+    
+    function manipulate()public returns (uint){
+    uint sum;
+     for(uint i=0;i<m_pairsOfFlags.length;i++){
+         uint inner = m_pairsOfFlags[i].length;
+          for(uint j=0;j<inner;j++){
+              sum = sum + m_pairsOfFlags[i][j];
+          }
+     }   
+     return sum;
+    }
+}
+
+        
