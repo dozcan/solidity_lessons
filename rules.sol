fc122e2096ad753c2b2098bd411535336ac1882a
doga ozcan <dogasava@gmail.com>
2020-10-16 18:51:28 +0300

    Create rules.sol

---
+++ rules.sol
@@ -0,0 +1,7 @@
+bytes2 a = 54321; // not allowed
+bytes2 b = 0x12; // not allowed
+bytes2 c = 0x123; // not allowed
+bytes2 d = 0x1234; // fine
+bytes2 e = 0x0012; // fine
+bytes4 f = 0; // fine
+bytes4 g = 0x0; // fine
